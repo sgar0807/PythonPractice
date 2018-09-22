6b9163cb31337fc1c8a9ec77183df6ccf28acb6e
Samuel Garcia <sgar0807@gmail.com>
2018-09-22 02:40:32 -0400

    Print line + user input + string cat.

---
+++ venv/main.py
@@ -0,0 +1,6 @@
+name = ""
+print("Welcome to Python, thank you for attending")
+print("I am learning to write Python")
+print("Please enter your name: ")
+name = input()
+print("Thank you, " + name + ", please carry on coding and have a good day.")
